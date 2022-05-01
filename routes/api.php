e6dfe3c1c0037d82afe9ac49846726d46b07ee7b
kazuyaTakahashi1988 <kazuya.takahashi.631130@gmail.com>
2022-05-01 20:22:02 +0900

    SET

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
 Route::post('/posts/{post}/like', 'LikeController@like');
 Route::post('/posts/{post}/unlike', 'LikeController@unlike');
